a2ba1781b0e9581ad502547fb4e09a24660e3519
unknown <andrey_omsk@mail.ru>
2022-05-19 14:49:43 +0600

    '1905day2'

--- src/components/Bind/BindDiagram.tsx
+++ src/components/Bind/BindDiagram.tsx
@@ -10,7 +10,7 @@ const BindDiagram = () => {
     <YMaps>
       <div>
         My awesome application with maps!
-        <Map defaultState={{ center: [55.75, 37.57], zoom: 50 }} />
+        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
       </div>
     </YMaps>
   );
